9a48a073549cf14c64499cb78d12a706f58f5749
devrusty <rguillory246@gmail.com>
2022-06-14 06:48:29 -0500

    added statuses; made changes to profile page

--- usersdb.js
+++ usersdb.js
@@ -4,6 +4,7 @@ module.exports = [
         id: 1,
         username: "admin",
         password: "admin",
+        status: "testing the statuses?",
         robux: 100,
         tickets: 2000,
         avatarCdn: "https://tr.rbxcdn.com/c4265017c98559993061733b1125a23c/150/150/AvatarHeadshot/Png"
