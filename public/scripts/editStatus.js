45cbd0e278bc42d297f55279f7e5acda3eab7667
devrusty <rguillory246@gmail.com>
2022-06-15 05:14:26 -0500

    fixed profile.ejs and editStatus.ejs

--- public/scripts/editStatus.js
+++ public/scripts/editStatus.js
@@ -1,16 +1,18 @@
 const profileStatus = document.querySelector("#profile-status")
 
-let editing = false
-const ToggleStatusEdit = () => {
-    if (!editing) {
-        const editingInput = document.createElement("input")
-        editingInput.value = profileStatus.value
-        editingInput.classList.add("profile-status")
-        editingInput.style.cursor = "pointer"
+if (profileStatus) {
+    let editing = false
+    const ToggleStatusEdit = () => {
+        if (!editing) {
+            const editingInput = document.createElement("input")
+            editingInput.value = profileStatus.innerHTML
+            editingInput.classList.add("profile-status")
+            editingInput.style.cursor = "pointer"
 
-        profileStatus.replaceWith(editingInput)
-        editing = true
+            profileStatus.replaceWith(editingInput)
+            editing = true
+        }
     }
-}
 
-profileStatus.addEventListener("click", (event) => ToggleStatusEdit())
+    profileStatus.addEventListener("click", (event) => ToggleStatusEdit())
+}
