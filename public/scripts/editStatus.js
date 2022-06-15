82473b9c0d8f1d0e1362a91483b7cfee03fa0b09
devrusty <rguillory246@gmail.com>
2022-06-15 05:11:50 -0500

    created editStatus.js

--- public/scripts/editStatus.js
+++ public/scripts/editStatus.js
@@ -1,11 +1,16 @@
 const profileStatus = document.querySelector("#profile-status")
 
 let editing = false
-
-profileStatus.addEventListener("click", (event) => {
+const ToggleStatusEdit = () => {
     if (!editing) {
+        const editingInput = document.createElement("input")
+        editingInput.value = profileStatus.value
+        editingInput.classList.add("profile-status")
+        editingInput.style.cursor = "pointer"
+
+        profileStatus.replaceWith(editingInput)
         editing = true
-        profileStatus.className = "input"
-        return
     }
-})
+}
+
+profileStatus.addEventListener("click", (event) => ToggleStatusEdit())
