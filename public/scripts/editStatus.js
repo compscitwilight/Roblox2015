542c88fb221329c1f98b31a4b0dc787a6a5d34d2
devrusty <rguillory246@gmail.com>
2022-06-15 05:01:33 -0500

    fixed if statement for authentication checking

--- public/scripts/editStatus.js
+++ public/scripts/editStatus.js
@@ -1,1 +1,11 @@
 const profileStatus = document.querySelector("#profile-status")
+
+let editing = false
+
+profileStatus.addEventListener("click", (event) => {
+    if (!editing) {
+        editing = true
+        profileStatus.className = "input"
+        return
+    }
+})
