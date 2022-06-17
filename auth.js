4e46023c05a07316491e787acb665da0de2b04ce
devrusty <rguillory246@gmail.com>
2022-06-17 11:49:33 -0500

    created auth.js to store authentication methods

---
+++ auth.js
@@ -0,0 +1,35 @@
+const users = require("./usersdb")
+
+module.exports = {
+    RegisterUser(data) {
+        if (data.username.length > 20) {
+            res.status(403).send("Username exceeds limit of 20 characters")
+            return
+        }
+        if (data.username.includes(" ")) {
+            res.status(403).send("Username includes a space, which is not allowed")
+            return
+        }
+        if (data.username.startsWith(body.username.match(/[0-9]/g))) {
+            res.status(403).send("Username cannot start with a number")
+            return
+        }
+        users.push({
+            id: users.length + 1,
+            username: data.username,
+            password: data.password,
+            status: "",
+            robux: 0,
+            tickets: 10,
+            avatarCdn: "https://tr.rbxcdn.com/c4265017c98559993061733b1125a23c/150/150/AvatarHeadshot/Png",
+            moderation: {
+                moderated: false,
+                note: "",
+                length: "",
+                reviewed: ""
+            }
+        })
+
+        return users.find(user => user.username == data.username)
+    }
+}
