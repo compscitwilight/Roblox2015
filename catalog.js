e2e02059453c88d68eade7adb18745759691f4a4
devrusty <rguillory246@gmail.com>
2022-06-18 10:09:04 -0500

    created create endpoint

--- catalog.js
+++ catalog.js
@@ -15,5 +15,27 @@ router.get("/:itemId", (req, res) => {
 })
 
 // post requests
+router.post("/create", (req, res) => {
+    const session = req.session
+    if (!session.authenticated || !session.user.admin) {
+        res.sendStatus(403)
+        return
+    }
+
+    const body = req.body
+    const newItem = {
+        id: catalogdb.length + 1,
+        name: body.name || "Unnamed",
+        price: body.price || { robux: 0, tickets: 0 },
+        onsale: body.onsale || false,
+        description: body.description || "This item does not have a description.",
+        creator: body.creator || "admin",
+        created: new Date().toUTCString(),
+        limited: body.limited || false,
+        limitedU: body.limitedU || false
+    }
+    catalogdb.push(newItem)
+    res.status(200).send(newItem)
+})
 
 module.exports = router
