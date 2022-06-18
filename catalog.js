cd9cb8e17400745bbcaca7c4bf94aadee5dd84ef
devrusty <rguillory246@gmail.com>
2022-06-18 08:07:17 -0500

    created catalog item get endpoint

--- catalog.js
+++ catalog.js
@@ -1,4 +1,19 @@
 const express = require("express")
 const router = express.Router()
 
+const catalogdb = require("./db/catalogdb.js")
+
+// get requests
+router.get("/:itemId", (req, res) => {
+    const itemId = req.params.itemId
+    const item = catalogdb.find(item => item.id == itemId)
+    if (!item) {
+        res.status(404).redirect("/catalog")
+        return
+    }
+    res.render("catalog.ejs", { session: req.session, item: item })
+})
+
+// post requests
+
 module.exports = router
