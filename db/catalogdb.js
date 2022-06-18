906ae1c0ccb7ec9dfd592b808ef653b13b2c80f0
devrusty <rguillory246@gmail.com>
2022-06-18 08:00:20 -0500

    created catalogdb file for storing catalog items

---
+++ db/catalogdb.js
@@ -0,0 +1,13 @@
+module.exports = [
+    {},
+    {
+        id: 1,
+        name: "TestHat",
+        price: { robux: 100, tickets: 0 },
+        description: "This is a hat just for testing!",
+        creator: "admin",
+        created: "6/18/2022 5:59AM PST",
+        limited: false,
+        limitedU: false
+    }
+]
