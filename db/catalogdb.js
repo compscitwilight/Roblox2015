8f35fd0f925edd470453ccd5ae5f6adfe1aea4b2
devrusty <rguillory246@gmail.com>
2022-06-18 08:03:10 -0500

    added onsale field to catalog items

--- db/catalogdb.js
+++ db/catalogdb.js
@@ -4,6 +4,7 @@ module.exports = [
         id: 1,
         name: "TestHat",
         price: { robux: 100, tickets: 0 },
+        onsale: true,
         description: "This is a hat just for testing!",
         creator: "admin",
         created: "6/18/2022 5:59AM PST",
