079c364f16acd5e1adfcad62b3fbc55fad72978d
devrusty <rguillory246@gmail.com>
2022-06-19 07:55:33 -0500

    created catalog/purchase endpoint

--- catalog.js
+++ catalog.js
@@ -39,4 +39,34 @@ router.post("/create", (req, res) => {
     res.status(200).redirect("/catalog/" + newItem.id)
 })
 
+router.post("/purchase", (req, res) => {
+    const body = req.body
+    const session = req.session
+    if (!session.authenticated) {
+        res.status(403)
+        return
+    }
+    if (!body.item) {
+        res.status(500)
+        return
+    }
+    const user = session.user
+    const item = catalogdb.find(item => item.name == body.item)
+    if (!item) {
+        res.status(404)
+        return
+    }
+    if (item.price.robux && user.robux > item.price.robux) {
+        user.robux -= item.price.robux
+        res.status(200)
+        return
+    }
+    if (item.price.tickets && user.tickets > item.price.tickets) {
+        user.tickets -= item.price.tickets
+        res.status(200)
+        return
+    }
+    res.status(500)
+})
+
 module.exports = router
